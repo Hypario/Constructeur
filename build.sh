6fa95e8c6e9935f5f3281862d78cb36c34331cf0
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-03-02 08:56:10 +0100

    make the build fail if the compilation fails

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-
+# if bin directory does not exists, create it
 if [ ! -d "bin" ]; then
    mkdir bin
 fi
-
+exitcode=0
+# retrieve java files to compile
 find src -name *.java -print >javafiles
+# compile the files for java 8+
 javac -d bin -source 8 -target 8 @javafiles
+exitcode=$((exitcode + $?))
+# create an autoexceutable jar file
 jar -cvfe builder-${USER}.jar Main -C bin/ .
+exitcode=$((exitcode + $?))
+# breaks the build if exitcode>0
+exit $exitcode
