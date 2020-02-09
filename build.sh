e20ee625bb36edd118b4a17518c2bc0bebd7e535
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2020-02-09 18:42:05 +0100

    again en anglais

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ exitcode=0
 # retrieve java files to compile
 find src -name *.java -print >javafiles
 # compile the files for java 8+
-export LANG=fr_FR.utf8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 javac -d bin -source 8 -target 8 @javafiles
 exitcode=$((exitcode + $?))
 # create an autoexceutable jar file
